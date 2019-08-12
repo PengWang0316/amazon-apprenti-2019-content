bc12bed28255a15d6c6563aa67ff39a43de8cc90
Kevin Wang <rocwang0316@gmail.com>
2019-08-12 13:42:02 -0700

    Run tsc

--- CS224/Kevin/Module2/public/app.js
+++ CS224/Kevin/Module2/public/app.js
@@ -4,7 +4,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var order_controller_1 = __importDefault(require("./controllers/order-controller"));
 var app = express_1.default();
 // The router
 app.get('/', function (req, res) { return res.end('This is your Express server.'); });
+app.get('/order', order_controller_1.default);
 app.listen(3000, function () { return console.log('The server is running on http://localhost:3000'); });
---
+++ CS224/Kevin/Module2/public/controllers/order-controller.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var handler = function (req, res) { return res.end('This is the order page'); };
+exports.default = handler;
